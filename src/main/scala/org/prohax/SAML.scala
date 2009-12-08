1a985f112e096a7cc43d9dd4b4bc99f889e01b19
Glen Maddern <glenmaddern@gmail.com>
2009-12-09 00:35:17 +1100

    omg

--- src/main/scala/org/prohax/SAML.scala
+++ src/main/scala/org/prohax/SAML.scala
@@ -4,42 +4,51 @@ import collection.mutable.{HashMap, Map}
 import java.lang.String
 
 object Tags {
-  case class Tag(ts: List[Tag]) {
+  case class Tag(tags: Any*) {
     val name = getClass.getSimpleName
 
-    override def toString = if (ts.isEmpty) {
+    override def toString = if (tags.isEmpty) {
       "<" + name + "></" + name + ">"
     } else {
-      "<" + name + ">\n" + ts.map(_.toString).mkString("\n") + "\n</" + name + ">"
+      "<" + name + ">\n" + tags.map(_.toString).mkString("\n") + "\n</" + name + ">"
     }
   }
-  
-  case class div() extends Tag(Nil)
-  case class html(t: Tag) extends Tag(List(t))
-  case class head(t: Tag) extends Tag(List(t))
-  case class body(t: Tag) extends Tag(List(t))
-}
 
-object SAML {
-  import Tags._
-  val tags = new HashMap[String, Tag]
-
-  def register(name: String, f: () => Tag) {
-    tags += (name -> f())
+  case class div() extends Tag()
+  case class html(ts: Tag*) extends Tag(ts: _*)
+  case class head(ts: Tag*) extends Tag(ts: _*)
+  case class body(ts: Tag*) extends Tag(ts: _*)
+  case class title(s: String) extends Tag(s)
+  case class h1(ts: Tag*) extends Tag(ts: _*)
+  case class h2(attr: (Symbol, String), ts: Tag*) extends Tag(ts: _*)
+  case class p(attr: (Symbol, String), ts: Tag*) extends Tag(ts: _*)
+  object p {
+    def apply(ts: Tag*): p = apply('n -> "", ts: _*)
   }
 
-  def render(name: String) {
-    println(tags(name))
+  implicit def stringToTag(s: String) = new Tag {
+    override def toString = s
   }
 }
+
+case class Post(username: String, title: String, body: String)
 
 object Main {
   import Tags._
-  def main(args: Array[String]) = {
-    SAML.register("simple", () => {
-      html(head(div()))
-    })
+  def main(args: Array[String]) {
+    println(render(Post("glen", "opinions", "wack.")))
+  }
 
-    SAML.render("simple")
+  def render(post: Post) = {
+    html(
+      head(
+        title(post.title)
+        ),
+      body(
+        h1(post.title, p("foo")),
+        h2('class -> "user", post.username),
+        p('class -> "post", post.body)
+        )
+      )
   }
 }
