16e0dc927a2a58329e25c11e6b808237c4ad6fa3
Glen Maddern <glenmaddern@gmail.com>
2009-12-14 18:22:43 +1100

    Starting down new idea, using more of a dsl than function calls

--- src/main/scala/org/prohax/Main.scala
+++ src/main/scala/org/prohax/Main.scala
@@ -2,13 +2,34 @@ package org.prohax
 
 import collection.mutable.{HashMap, Map}
 import java.lang.String
+import scala.xml.{TopScope, Elem}
 
 case class Post(username: String, title: String, body: String)
 
 object Main {
   def main(args: Array[String]) {
     val post = Post("glen", "opinions", "wack.")
-    println(render(post))
+    println(render2(post))
+  }
+
+  trait SymbolTag {
+    val classes: Seq[String]
+    val id: Option[String]
+    val name: String
+
+    def elem = Elem(null, name, null, TopScope)
+
+    override def toString = elem.toString
+  }
+
+  implicit def toSymbolTag(s: Symbol) = new SymbolTag {
+    val classes = Nil
+    val id = None
+    val name = s.name
+  }
+
+  def render2(post: Post): SymbolTag = {
+    'head //c 'class id 'head
   }
 
   def render(post: Post) = {
