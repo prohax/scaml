bf769d7a2dc46db7a44ac1d3c6b4d5c0ae9f7528
Glen Maddern <glenmaddern@gmail.com>
2009-12-15 01:41:38 +1100

    Expanded example. It's working pretty well!

--- src/main/scala/org/prohax/Main.scala
+++ src/main/scala/org/prohax/Main.scala
@@ -1,6 +1,6 @@
 package org.prohax
 
-import Predef.{stringWrapper => _,_}
+import Predef.{stringWrapper => _, _}
 import java.lang.String
 import collection.mutable.ArrayBuffer
 
@@ -8,17 +8,18 @@ case class Post(username: String, title: String, body: String)
 
 object Main {
   def main(args: Array[String]) {
-    val post = Post("glen", "opinions", "wack.")
-    println(render3(post))
+    val post = Post("Mr Some Lolfase", "I Haz A Opinion", "And it's lolled.")
+    val post2 = Post("glen", "Why I Win", "QED")
+    println(render3(post, post2))
   }
 
   def render2(post: Post): NewScaml.SymbolTag = {
     import NewScaml._
-    
+
     'head c 'winpants id 'head
   }
 
-  def render3(post: Post) = {
+  def render3(posts: Post*) = {
     import Scaml._
 
     implicit val tags = new ArrayBuffer[Tag]
@@ -31,30 +32,24 @@ object Main {
       }
       "body" {
         "h1.header"('href -> "http://lol.fase") {
-          "Hello, " + post.username
+          "Hello, " + posts.first.username
         }
-        "div.spacer#spacerMan" { }
+        "div.spacer#spacerMan" {}
+        "ol" {
+          posts.foreach {post =>
+            "li" {
+              "h2.title" {post.title}
+              "h6.username" {post.username}
+              "p.body" {post.body}
+            }
+          }
+        }
       }
     }
 
     tags.first
 
   }
-//      "head"
-//      "body.class1.class2" {
-//        "ul#posts" {
-//          posts.map { post =>
-//            "li.post" {
-//              post.body
-//            }
-//          }
-//        }
-//        "a.blah"('href -> "lol") {
-//          "link text"
-//        }
-//      }
-//    }
-//  }
 
   def render(post: Post) = {
     import Scaml._
