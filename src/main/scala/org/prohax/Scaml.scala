2115f4ea5a9112829829901c6943c48dbdcb998d
Glen Maddern <glenmaddern@gmail.com>
2009-12-14 17:42:37 +1100

    fixed new way, more reuse too

--- src/main/scala/org/prohax/Scaml.scala
+++ src/main/scala/org/prohax/Scaml.scala
@@ -38,32 +38,27 @@ object Scaml {
   }
 
   case class TagBuilder(n: String) {
-    def apply() = new TagV2 {
-      val attrs: Map[Symbol, String] = Map()
-      val name: String = n
-      val tags: Seq[Tag] = Seq()
-    }
-
-    def apply(ts: Tag*) = new TagV2 {
-      val attrs: Map[Symbol, String] = Map()
-      val name = n
-      val tags: Seq[Tag] = ts
-    }
-
-    def apply(as: Map[Symbol, String], ts: Tag*) = new TagV2 {
+    def apply(as: Map[Symbol, String], ts: Seq[Tag]) = new TagV2 {
       val attrs: Map[Symbol, String] = as
       val name = n
       val tags: Seq[Tag] = ts
     }
 
-    def apply(a1: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1))
+    def apply(as: Map[Symbol, String], ts: Tag*): TagV2 = apply(as, ts)
 
-    def apply(a1: (Symbol, String), a2: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1, a2))
+    def apply(): TagV2 = apply(Map[Symbol, String]())
 
-    def apply(a1: (Symbol, String), a2: (Symbol, String), a3: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1, a2, a3))
+    def apply(ts: Tag*): TagV2 = apply(Map[Symbol, String](), ts)
+
+    def apply(a1: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1), ts)
+
+    def apply(a1: (Symbol, String), a2: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1, a2), ts)
+    def apply(a1: (Symbol, String), a2: (Symbol, String), a3: (Symbol, String), ts: Tag*): TagV2 = apply(Map(a1, a2, a3), ts)
   }
 
-  val List(html, head, title, body, h1, h2, p, span, div) = List("html", "head", "title", "body", "h1", "h2", "p", "span", "div").map(TagBuilder(_))
+  val List(html, head, title, body, h1, h2, p, span, div) = List(
+    "html", "head", "title", "body", "h1", "h2", "p", "span", "div"
+    ).map(TagBuilder(_))
 
   implicit def toOldTag(t: TagV2): Tag = new Tag(t.attrs, t.tags: _ *) {
     override val name = t.name
