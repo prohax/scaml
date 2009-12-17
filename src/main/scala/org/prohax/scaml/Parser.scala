7c99a6b42f9f5dafeb323203cf918192f7c64b54
Glen Maddern <glenmaddern@gmail.com>
2009-12-17 18:37:08 +1100

    got the doctype declaration in

--- src/main/scala/org/prohax/scaml/Parser.scala
+++ src/main/scala/org/prohax/scaml/Parser.scala
@@ -1,11 +1,18 @@
 package org.prohax.scaml
 
-import scala.xml.{Text, NodeSeq}
+import scala.xml.{Text, NodeSeq, Unparsed}
+import scala.util.parsing.combinator._
 
-trait ScamlFile {
-  def render: NodeSeq
+object Constants {
+  val DOCTYPE = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">"""
 }
 
 object Parser {
-  def parse(input: String): ScamlFile = new ScamlFile { def render = Text("")}
+  private val foo = new Foo
+
+  def parse(input: String) = foo.parseAll(foo.header, input).getOrElse("")
+}
+
+class Foo extends JavaTokenParsers {
+  def header: Parser[String] = "!!!".r ^^ (_ => Constants.DOCTYPE)
 }
--- src/test/scala/org/prohax/scaml/ScamlParserSpec.scala
+++ src/test/scala/org/prohax/scaml/ScamlParserSpec.scala
@@ -3,9 +3,15 @@ package org.prohax.scaml
 import org.specs._
 
 object ScamlParserSpec extends Specification {
+  def p(s: String) = Parser.parse(s)
   "The parser" should {
-    "do some simple stuff" in {
-      Parser.parse("").render.toString must beEqualTo("")
+    "work with whitespace" in {
+      p("") must beEqualTo("")
+      p("           ") must beEqualTo("")
+      p("\n" * 10) must beEqualTo("")
+    }
+    "generate xml headers like haml" in {
+      p("!!!") must beEqualTo(Constants.DOCTYPE)
     }
   }
 }
