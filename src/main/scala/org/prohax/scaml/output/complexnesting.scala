3e130578b2f70084c71bd25e60015d3bf3e34607
Glen Maddern <glenmaddern@gmail.com>
2009-12-26 01:30:37 +1100

    added more complex test

---
+++ src/main/scala/org/prohax/scaml/output/complexnesting.scala
@@ -0,0 +1,26 @@
+package org.prohax.scaml.output
+
+import scala.xml._
+import org.prohax.scaml.ScamlFile
+
+object complexnesting extends ScamlFile {
+  def render() = {
+    <html>
+      <head>
+        <title/>
+      </head>
+      <body>
+        <div>
+          <h1/>
+          <div>
+            <p>
+              <strong/>
+            </p>
+            <p/>
+          </div>
+        </div>
+        <div/>
+      </body>
+    </html>
+  }
+}
