aa932788f5fbbd18fc712ee39338437bf54631bb
Glen Maddern <glenmaddern@gmail.com>
2009-12-27 20:06:29 +1100

    and tested its output, looking good!

--- src/test/scala/org/prohax/scaml/ScamlParserSpec.scala
+++ src/test/scala/org/prohax/scaml/ScamlParserSpec.scala
@@ -31,7 +31,8 @@ object ScamlParserSpec extends Specification {
       ("doublynested", () => doublynested.renderString),
       ("emptyfile", () => emptyfile.renderString),
       ("html", () => html.renderString),
-      ("literals", () => literals.renderString)
+      ("literals", () => literals.renderString),
+      ("tag_parsing", () => tag_parsing.renderString)
     ).foreach(x => {
       "work for " + x._1 in {
         x._2() must beEqualTo(read(outputDir + x._1 + ".html"))
