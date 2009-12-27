df7b46dd296ab15157a88718329b374eb51257c5
Glen Maddern <glenmaddern@gmail.com>
2009-12-27 19:29:49 +1100

    oh yeah spec passing for inline and block text

--- src/main/scala/org/prohax/scaml/ScamlObjects.scala
+++ src/main/scala/org/prohax/scaml/ScamlObjects.scala
@@ -1,7 +1,11 @@
 package org.prohax.scaml
 
 case class ScamlTag(level: Int, name: Option[String], id: Option[String], classes: List[String], text: Option[String]) {
-  def isEmpty = name.isEmpty && id.isEmpty && classes.isEmpty && text.isEmpty
+  def isEmpty = !isTag && text.isEmpty
+
+  def isText = !isTag && !text.isEmpty
+
+  def isTag = !(name.isEmpty && id.isEmpty && classes.isEmpty)
 }
 object ScamlTag {
   def apply(level: Int, name: String) = new ScamlTag(level, Some(name), None, Nil, None)
@@ -9,15 +13,29 @@ object ScamlTag {
   def apply(level: Int, name: String, id: String) = new ScamlTag(level, Some(name), Some(id), Nil, None)
 
   def apply(level: Int, name: String, id: Option[String]) = new ScamlTag(level, Some(name), id, Nil, None)
-  
+
   def apply(level: Int, name: String, id: Option[String], classes: List[String]) = new ScamlTag(level, Some(name), id, classes, None)
 }
 
 case class NestedTag(tag: ScamlTag, subtags: List[NestedTag]) {
-  def toStringWithIndent(indent: Int): String = if (tag.isEmpty) "" else Constants.indent(indent) + {
-    val name = tag.name.getOrElse("div")
-    if (subtags.isEmpty) "<" + name + attrs + "/>" else {
-      "<" + name + attrs + ">\n" + subtags.reverseMap(_.toStringWithIndent(indent + 1)).mkString("\n") + "\n" + Constants.indent(indent) + "</" + name + ">"
+  def toStringWithIndent(indent: Int): String = {
+    if (tag.isEmpty) {
+      ""
+    } else {
+      Constants.indent(indent) + {
+        if (tag.isText) {
+          tag.text.get
+        } else {
+          val name = tag.name.getOrElse("div")
+          val within = tag.text.getOrElse({
+            if (subtags.isEmpty) "" else
+              "\n" + subtags.reverseMap(_.toStringWithIndent(indent + 1)).mkString("\n") + "\n" + Constants.indent(indent)
+          })
+          if (within.isEmpty) "<" + name + attrs + "/>" else {
+            "<" + name + attrs + ">" + within + "</" + name + ">"
+          }
+        }
+      }
     }
   }
 
