80545411ecdb7bfbd1c82e091d98ef785d34263e
Glen Maddern <glenmaddern@gmail.com>
2009-12-29 16:26:50 +1100

    locally publishable now under company prohax

--- project/build/GitwatchProject.scala
+++ project/build/GitwatchProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class GitwatchProject(info: ProjectInfo) extends DefaultWebProject(info) {
+class GitwatchProject(info: ProjectInfo) extends DefaultProject(info) {
   import BasicScalaProject._
   
   val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
